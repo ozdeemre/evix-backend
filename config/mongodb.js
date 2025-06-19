a30cfe0eea03bf5df0b69aef39a5a9e730239b73
Emre ÖZDEMİR <emre@Akia.pvt>
2025-06-19 11:38:54 +0300

    Add .env to .gitignore and remove it from git tracking

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    throw new Error('MONGODB_URI environment variable is missing!');
+  }
 
-    mongoose.connection.on('connected', () => {
-        console.log('DB Connected')
-    })
+  mongoose.connection.on('connected', () => {
+    console.log('DB Connected');
+  });
 
-    await mongoose.connect(process.env.MONGODB_URI);
-
-}
+  await mongoose.connect(uri);
+};
 
 export default connectDB;
